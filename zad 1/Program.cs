60de23bf090bb4a0b3b0254982a4433fc1632e4b
Jivko Karov <jikito2007@gmail.com>
2025-03-10 11:24:55 +0200

    Add project files.

---
+++ zad 1/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Lists__SLL_and_DLL_
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
